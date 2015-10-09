9a6d78a308f864d7892ac15833f8d92b0a324b40
hidemon <yusun@utexas.edu>
2015-10-08 19:54:12 -0500

    bug fix

--- js/upload.js
+++ js/upload.js
@@ -1,4 +1,3 @@
-
 Dropzone.autoDiscover = true;
 
 var dropzone;
@@ -17,96 +16,76 @@ Dropzone.options.myDropzone = {
     }
 };
 
-$(function() {
+$(function () {
     $("#uploadBtn").click(function () {
 
         document.getElementById("response").innerHTML = "Uploading . . .";
 
         var i = 0, len = dropzone.files.length;
         window.alert(len);
-        for (; i < len; i++) {
-            console.error("before");
-            file = dropzone.files[i];
-            console.error("after");
-            $.ajax({
-                url: window.location.href,
-                type: "POST",
-                dataType: 'text',
-                async: false,
-                success: function (res) {
-                    dropzone.options.url = res;
-                    dropzone.processFile(file);
-                    
-                }
+        files = dropzone.files;
+
+        //for (; i < len; i++) {
+        //    console.error("before");
+        //    file = dropzone.files[i];
+        //    console.error("after");
+        //    $.ajax({
+        //        url: window.location.href,
+        //        type: "POST",
+        //        dataType: 'text',
+        //        async: false,
+        //        success: function (res) {
+        //            dropzone.options.url = res;
+        //            dropzone.processFile(file);
+        //
+        //
+        //        }
+        //    });
+        //    console.error("after ajax");
+        //}
+
+        //$.ajax({
+        //    url: window.location.href,
+        //    type: "POST",
+        //    dataType: 'text',
+        //    async: false,
+        //    success: function (res) {
+        //        dropzone.options.url = res;
+        //        dropzone.processFile(files[0]);
+        //        i++;
+        //    }
+        //});
+         $.ajax({
+                    url: window.location.href,
+                    type: "POST",
+                    dataType: 'text',
+                    async: false,
+                    success: function (res) {
+                        dropzone.options.url = res;
+                        dropzone.processFile(files[0]);
+                        i++;
+                    }
+                });
+
+        dropzone.on("complete", function() {
+            if (i < len) {
+                $.ajax({
+                    url: window.location.href,
+                    type: "POST",
+                    dataType: 'text',
+                    async: false,
+                    success: function (res) {
+                        dropzone.options.url = res;
+                        dropzone.processFile(files[i]);
+                        i++;
+                    }
+                });
+            }
+        });
 
 
-            });
-            console.error("after ajax");
-        }
 
     });
+
 });
-
-//var input = $('[name="file"]'),
-//    formdata = false;
-//
-//function showUploadedItem(source) {
-//    var list = document.getElementById("image-list"),
-//        li = document.createElement("li"),
-//        img = document.createElement("img");
-//    img.src = source;
-//    li.appendChild(img);
-//    list.appendChild(li);
-//}
-//
-//
-//
-//if (window.FormData) {
-//    formdata = new FormData();
-////            document.getElementById("btn").style.display = "none";
-//}
-//input.addEventListener("change", function (evt) {
-//     window.alert("a change");
-//    document.getElementById("response").innerHTML = "Uploading . . .";
-//    var i = 0, len = dropzone.files.length, img, reader, file;
-//
-//    for (; i < len; i++) {
-//        file = this.files[i];
-//
-//        if (!!file.type.match(/image.*/)) {
-//            if (window.FileReader) {
-//                reader = new FileReader();
-//                reader.onloadend = function (e) {
-//                    showUploadedItem(e.target.result, file.fileName);
-//                };
-//                reader.readAsDataURL(file);
-//            }
-//            if (formdata) {
-//                formdata.append("images[]", file);
-//            }
-//        }
-//    }
-//
-//    if (formdata) {
-//
-//        $.ajax({
-//            url: "{{upload_url}}",
-//            type: "POST",
-//            data: formdata,
-//            processData: false,
-//            contentType: false,
-//            success: function (res) {
-//                dropzone.options.url = res;
-////                        $("#my-awesome-dropzone").attr(action, res);
-//                for (file in dropzone.files)
-//                    dropzone._processFile(file);
-//                //document.getElementById("response").innerHTML = res;
-//            }
-//
-//
-//        });
-//    }
-//
-//}, false);
-
 
