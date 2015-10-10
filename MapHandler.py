67d6d3a0b7aad7572e57288a5d69119266e3850e
hidemon <yusun@utexas.edu>
2015-10-10 14:44:42 -0500

    Fix bugs

--- MapHandler.py
+++ MapHandler.py
@@ -1,4 +1,6 @@
 import json
+import logging
+import random
 from google.appengine.api.images import get_serving_url
 import webapp2
 from google.appengine.api import users
@@ -34,17 +36,15 @@ class MapHandler(webapp2.RequestHandler):
       stream = Stream.query(Stream.name == stream_name).fetch(1)[0]
 
 
-      raw_data = { "count": 4,
-      "photos": [{"photo_id": 27932, "photo_title": "Atardecer en Embalse", "photo_url": "http://www.panoramio.com/photo/27932", "photo_file_url": get_serving_url(stream.blob_key[0]), "longitude": -64.404945, "latitude": -32.202924, "width": 500, "height": 375, "upload_date": "25 June 2006", "owner_id": 4483, "owner_name": "Miguel Coranti", "owner_url": "http://www.panoramio.com/user/4483"}
-      ,
-      {"photo_id": 522084, "photo_title": "In Memoriam Antoine de Saint Exupry", "photo_url": "http://www.panoramio.com/photo/522084", "photo_file_url": "http://mw2.google.com/mw-panoramio/photos/medium/522084.jpg", "longitude": 17.470493, "latitude": 47.867077, "width": 500, "height": 350, "upload_date": "21 January 2007", "owner_id": 109117, "owner_name": "Busa Pter", "owner_url": "http://www.panoramio.com/user/109117"}
-      ,
-      {"photo_id": 1578881, "photo_title": "Rosina Lamberti,Sunset,Templestowe , Victoria, Australia", "photo_url": "http://www.panoramio.com/photo/1578881", "photo_file_url": "http://mw2.google.com/mw-panoramio/photos/medium/1578881.jpg", "longitude": 145.141754, "latitude": -37.766372, "width": 500, "height": 474, "upload_date": "01 April 2007", "owner_id": 140796, "owner_name": "rosina lamberti", "owner_url": "http://www.panoramio.com/user/140796"}
-      ,
-      {"photo_id": 7593894, "photo_title": "fff", "photo_url": "http://www.panoramio.com/photo/7593894", "photo_file_url": "http://mw2.google.com/mw-panoramio/photos/medium/7593894.jpg", "longitude": 110.424957, "latitude": 24.781747, "width": 500, "height": 375, "upload_date": "04 February 2008", "owner_id": 161470, "owner_name": "John Su", "owner_url": "http://www.panoramio.com/user/161470"}
+      raw_data = { "count": 0,
+      "photos": [
       ]}
       i = 1
-      # for image in stream.blob_key:
-      #     raw_data["photos"].append({"photo_id": i, "photo_title": "", "photo_url": get_serving_url(stream.blob_key[0]),"photo_file_url": get_serving_url(stream.blob_key[0]), "longitude": 100, "lattitude": 100 ,  "width": 500, "height": 375, "upload_date": "25 June 2006", "owner_id": 4483, "owner_name": "Miguel Coranti", "owner_url": "http://www.panoramio.com/user/4483"})
+
+      for image_url in stream.blob_key:
+        raw_data['photos'].append({"photo_id": i, "photo_title": "live", "photo_url": "http://www.panoramio.com/photo/7593894", "photo_file_url": get_serving_url(image_url), "longitude": random.randint(-100, 100), "latitude": random.randint(-100, 100), "width": 500, "height": 375, "upload_date": "04 February 2008", "owner_id": 161470, "owner_name": "John Su", "owner_url": "http://www.panoramio.com/user/161470"})
+        raw_data['count'] = raw_data['count'] + 1
+        i = i + 1
+
       data = json.dumps(raw_data)
       self.response.write(data)
