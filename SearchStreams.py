3fdc08cf7c57e5e8f81a2285596147a99868c460
Liheng DIng <dinglihengusapp@163.com>
2015-10-11 20:19:55 -0500

    change the search

--- SearchStreams.py
+++ SearchStreams.py
@@ -3,6 +3,7 @@ import logging
 __author__ = 'yusun'
 import cgi
 import os
+import re
 import urllib
 import jinja2
 import webapp2
@@ -45,7 +46,6 @@ class SearchStreams(webapp2.RequestHandler):
                 #         pass
                 #     else:
                 #         currentUser.streams_searched.remove(stream_key)
-                result_number = len(streams_key)
                 for stream_key in streams_key:
                     stream = stream_key.get()
                     if stream:
@@ -54,6 +54,7 @@ class SearchStreams(webapp2.RequestHandler):
                         currentUser = User(identity = user.user_id(), email = user.email())
                         currentUser.put()
                 keyword = currentUser.keyword
+                result_number = len(searched_streams)
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
@@ -71,17 +72,14 @@ class SearchStreams(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('/htmls/SearchStreams.html')
         self.response.write(template.render(template_values))
 
+
     def post(self):
         user = users.get_current_user()
-        owned_streams = []
-        searched_streams = []
-        documents = []
+        # documents = []
         if user:
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
             getUser = User.query(User.email == user.email())
-
-            # self.response.write(str(getUser.fetch(1)))
             if getUser.fetch(1):
 
                 currentUser = getUser.fetch(1)[0]
@@ -91,37 +89,64 @@ class SearchStreams(webapp2.RequestHandler):
                 currentUser.put()
                 currentUser.put()
                 streams = CreateStream.Stream.query().fetch(50)
+                pattern = re.compile('\S*'+keyword+'\S*',re.I)
+                # tokens = ','.join([keyword[:i] for i in xrange(1, len(keyword)+1)])
+                # tokens = tokens.split(',')
+                # for token in tokens[::-1]:
+                #     pattern = re.compile('\S*'+token+'\S*')
+                #     for stream in streams[:]:
+                #         content = stream.name
+                #         for tag in stream.tags:
+                #             content = content+' '+tag
+                #         match = pattern.search(content)
+                #         if match:
+                #             currentUser.streams_searched.append(stream.key)
+                #             streams.remove(stream)
                 for stream in streams:
-                    content = ""
+                    content = stream.name
                     for tag in stream.tags:
-                        content = content+" "+tag
-                    document = search.Document(
-                        doc_id = str(stream.key.id()),
-                        fields = [search.TextField(name = "streams_owned",value=stream.name+content)]
-                    )
-                    documents.append(document)
-                self.response.write(documents)
-                if documents:
-                    index = search.Index(name = 'streams_owned')
-                    docs = index.put(documents)
-                    try:
-                        results = index.search(currentUser.keyword)
-                        n = 0
-                        for result in results:
-                            if n < 5:
-                                for stream in streams:
-                                    if long(result.doc_id) == stream.key.id():
-                                        currentUser.streams_searched.append(stream.key)
-                                        n = n+1
-                            else:
-                                break
-                        currentUser.put()
-                        currentUser.put()
-                        # self.response.write(results)
-                        for doc in docs:
-                            index.delete(doc.id)
-                    except search.Error:
-                        logging.exception("Search failed")
+                        content = content+' '+tag
+                    match = pattern.search(content)
+                    if match:
+                        currentUser.streams_searched.append(stream.key)
+                if len(currentUser.streams_searched)>20:
+                    currentUser.streams_searched = currentUser.streams_searched[0:19]
+                currentUser.streams_searched.sort(key=lambda d:d.get().name.upper(),reverse=False)
+                currentUser.put()
+                currentUser.put()
+
+                # <----------fuck-------------------------------------------------------------------------------->
+            #     for stream in streams:
+            #         content = ""
+            #         for tag in stream.tags:
+            #             content = content+" "+tag
+            #         document = search.Document(
+            #             doc_id = str(stream.key.id()),
+            #             fields = [search.TextField(name = "streams_owned",value=stream.name+' '+content)]
+            #         )
+            #         documents.append(document)
+            #     self.response.write(documents)
+            #     if documents:
+            #         index = search.Index(name = 'streams_owned')
+            #         docs = index.put(documents)
+            #         try:
+            #             results = index.search(currentUser.keyword)
+            #             n = 0
+            #             for result in results:
+            #                 if n < 5:
+            #                     for stream in streams:
+            #                         if long(result.doc_id) == stream.key.id():
+            #                             currentUser.streams_searched.append(stream.key)
+            #                             n = n+1
+            #                 else:
+            #                     break
+            #             currentUser.put()
+            #             currentUser.put()
+            #             # self.response.write(results)
+            #             for doc in docs:
+            #                 index.delete(doc.id)
+            #         except search.Error:
+            #             logging.exception("Search failed")
             else:
                 currentUser = User(identity = user.user_id(), email = user.email())
                 currentUser.put()
@@ -130,13 +155,15 @@ class SearchStreams(webapp2.RequestHandler):
             url_linktext = 'Login'
 
         # for stream in currentUser.streams_owned:
-        template_values = {
-            'streams': searched_streams,
-            'user_id': currentUser.identity,
-            'url': url,
-            'url_linktext': url_linktext,
-        }
+        # template_values = {
+        #     'streams': searched_streams,
+        #     'user_id': currentUser.identity,
+        #     'url': url,
+        #     'url_linktext': url_linktext,
+        # }
         self.redirect('/search')
+
+
 
 
 
