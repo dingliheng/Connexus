712895e23305b15e8180e68c32285686eaca6895
Liheng DIng <dinglihengusapp@163.com>
2015-10-22 09:38:08 -0500

    add android ViewStreams

---
+++ Android_ViewStreams.py
@@ -0,0 +1,20 @@
+__author__ = 'Liheng'
+import webapp2
+from CreateStream import CreateNewStream
+import CreateStream
+import json
+
+class Android_ViewAllStreams(webapp2.RequestHandler):
+    def get(self):
+        streams = CreateStream.Stream.query().fetch(16)
+        streams = sorted(streams, key=lambda k: k.date,reverse = True)
+        streams_URLs = []
+        for stream in streams:
+            streams_URLs.append(stream.cover)
+        dictPassed = {'cover_urls':streams_URLs}
+        jsonObj = json.dumps(dictPassed,sort_keys=True,indent=4, separators=(',', ': '))
+        self.response.write(jsonObj)
+
+app = webapp2.WSGIApplication([
+                               ('/android_viewall', Android_ViewAllStreams),
+                               ], debug=True)
--- ConnexusAndroid/app/src/main/java/edu/utaustin/yusun/connexusandroid/ViewStreamsActivity.java
+++ ConnexusAndroid/app/src/main/java/edu/utaustin/yusun/connexusandroid/ViewStreamsActivity.java
@@ -1,5 +1,8 @@
 package edu.utaustin.yusun.connexusandroid;
 
+
+import android.content.Context;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,54 +12,76 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
-public class ViewStreamsActivity extends AppCompatActivity implements
-        View.OnClickListener {
-    static final String[] numbers = new String[] {
-            "A", "B", "C", "D", "E",
-            "F", "G", "H", "I", "J",
-            "K", "L", "M", "N", "O",
-            "P", "Q", "R", "S", "T",
-            "U", "V", "W", "X", "Y", "Z"};
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+import java.util.ArrayList;
+
+public class ViewStreamsActivity extends AppCompatActivity {
+//    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+    Context context = this;
+    private String TAG  = "Display Streams";
+    final String ViewAllStreams_url = "http://connexus-1104.appspot.com/android_viewall";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_streams);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ExpandableHeightGridView gridView = (ExpandableHeightGridView) findViewById(R.id.gridView);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, numbers);
+        AsyncHttpClient httpClient = new AsyncHttpClient();
+        httpClient.get(ViewAllStreams_url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                final ArrayList<String> cover_URLs = new ArrayList<String>();
+                try {
+                    System.out.println(new String(responseBody));
+                    JSONObject jObject = new JSONObject(new String(responseBody));
+                    JSONArray cover_URLs_json = jObject.getJSONArray("cover_urls");
 
-        gridView.setAdapter(adapter);
-        gridView.setExpanded(true);
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Intent k = new Intent(ViewStreamsActivity.this, ViewAStreamActivity.class);
-                startActivity(k);
+                    for (int i = 0; i < cover_URLs_json.length(); i++) {
+                        cover_URLs.add(cover_URLs_json.getString(i));
+                        System.out.println(cover_URLs_json.getString(i));
+                    }
+                    ExpandableHeightGridView gridView = (ExpandableHeightGridView) findViewById(R.id.gridView);
+                    gridView.setAdapter(new ImageAdapter(context, cover_URLs));
+                    gridView.setExpanded(true);
+                    gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                                        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                                                            Toast.makeText(getApplicationContext(),
+                                                                    ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
+                                                        }
+                                                    }
+                    );
+                } catch (JSONException j) {
+                    System.out.println("JSON Error");
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers,byte[] responseBody, Throwable error) {
+
             }
         });
-        // Set up button click listeners
-        findViewById(R.id.search).setOnClickListener(this);
-        findViewById(R.id.nearby).setOnClickListener(this);
-        findViewById(R.id.view_subscribed_streams).setOnClickListener(this);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
     }
 
     // [START on_click]
-    @Override
+//    @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.nearby:
@@ -80,12 +105,10 @@ public class ViewStreamsActivity extends AppCompatActivity implements
 
     //When click on the search button
     private void onSearchClicked() {
-
     }
 
     //When click on the view subscibed stream button
     private  void onViewSubscribedClicked() {
-
     }
 
 }
