33a72d92ed28daeb176bf126d88ecf4bd353b8de
hidemon <yusun@utexas.edu>
2015-10-23 16:23:56 -0500

    Drawer Fix phase1

---
+++ ConnexusAndroid/app/src/main/java/edu/utaustin/yusun/connexusandroid/ObjectDrawerItem.java
@@ -0,0 +1,17 @@
+package edu.utaustin.yusun.connexusandroid;
+
+/**
+ * Created by mackbook on 15/10/23.
+ */
+public class ObjectDrawerItem {
+
+    public int icon;
+    public String name;
+
+    // Constructor.
+    public ObjectDrawerItem(int icon, String name) {
+
+        this.icon = icon;
+        this.name = name;
+    }
+}
