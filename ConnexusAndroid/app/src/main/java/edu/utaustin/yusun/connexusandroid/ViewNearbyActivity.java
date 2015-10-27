98cdce749a8389fac57713fcfd78bd4e8c3d3596
hidemon <yusun@utexas.edu>
2015-10-26 19:40:35 -0500

    A new Location method

--- ConnexusAndroid/app/src/main/java/edu/utaustin/yusun/connexusandroid/ViewNearbyActivity.java
+++ ConnexusAndroid/app/src/main/java/edu/utaustin/yusun/connexusandroid/ViewNearbyActivity.java
@@ -1,44 +1,37 @@
 package edu.utaustin.yusun.connexusandroid;
 
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.location.LocationServices;
+import java.util.List;
 
 /**
  * Created by yusun on 15/10/18.
  */
-public class ViewNearbyActivity extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener{
-    protected static final String TAG = "MainActivity";
-
-    /**
-     * Provides the entry point to Google Play services.
-     */
-    protected GoogleApiClient mGoogleApiClient;
-
-    /**
-     * Represents a geographical location.
-     */
-    protected Location mLastLocation;
+public class ViewNearbyActivity extends AppCompatActivity implements LocationListener {
 
     protected String mLatitudeLabel;
     protected String mLongitudeLabel;
     protected TextView mLatitudeText;
     protected TextView mLongitudeText;
+
+
+    private static final String[] S = { "Out of Service",
+            "Temporarily Unavailable", "Available" };
+
+    private TextView output;
+    private LocationManager locationManager;
+    private String bestProvider;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_nearby);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -46,80 +39,94 @@ public class ViewNearbyActivity extends AppCompatActivity implements ConnectionC
 
         mLatitudeLabel = getResources().getString(R.string.latitude_label);
         mLongitudeLabel = getResources().getString(R.string.longitude_label);
-        mLatitudeText = (TextView) findViewById((R.id.latitude_text));
+//        mLatitudeText = (TextView) findViewById((R.id.latitude_text));
         mLongitudeText = (TextView) findViewById((R.id.longitude_text));
 
-        buildGoogleApiClient();
+        // Get the output UI
+        output = (TextView) findViewById(R.id.latitude_text);
 
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-    /**
-     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
-     */
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
+        // List all providers:
+        List<String> providers = locationManager.getAllProviders();
+        for (String provider : providers) {
+            printProvider(provider);
         }
-    }
 
-    /**
-     * Runs when a GoogleApiClient object successfully connects.
-     */
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        // Provides a simple way of getting a device's location and is well suited for
-        // applications that do not require a fine-grained location and that do not need location
-        // updates. Gets the best and most recent location currently available, which may be null
-        // in rare cases when a location is not available.
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLastLocation != null) {
-            mLatitudeText.setText(String.format("%s: %f", mLatitudeLabel,
-                    mLastLocation.getLatitude()));
-            mLongitudeText.setText(String.format("%s: %f", mLongitudeLabel,
-                    mLastLocation.getLongitude()));
-        } else {
-            Toast.makeText(this, R.string.no_location_detected, Toast.LENGTH_LONG).show();
+        Criteria criteria = new Criteria();
+        bestProvider = locationManager.getBestProvider(criteria, false);
+        output.append("\n\nBEST Provider:\n");
+        printProvider(bestProvider);
+
+        output.append("\n\nLocations (starting with last known):");
+        try {
+            Location location = locationManager.getLastKnownLocation(bestProvider);
+            printLocation(location);
+        } catch (SecurityException e) {
+
         }
+
     }
 
+    /** Register for the updates when Activity is in foreground */
     @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
-        // onConnectionFailed.
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    protected void onResume() {
+        super.onResume();
+        try {
+            locationManager.requestLocationUpdates(bestProvider, 1000, 1, this);
+        } catch (SecurityException e) {
+
+        }
+
     }
 
+    /** Stop the updates when Activity is paused */
     @Override
-    public void onConnectionSuspended(int cause) {
-        // The connection to Google Play services was lost for some reason. We call connect() to
-        // attempt to re-establish the connection.
-        Log.i(TAG, "Connection suspended");
-        mGoogleApiClient.connect();
+    protected void onPause() {
+        super.onPause();
+        try {
+            locationManager.removeUpdates(this);
+        } catch (SecurityException e) {
+
+        }
+
     }
+
+    public void onLocationChanged(Location location) {
+        printLocation(location);
+    }
+
+    public void onProviderDisabled(String provider) {
+        // let okProvider be bestProvider
+        // re-register for updates
+        output.append("\n\nProvider Disabled: " + provider);
+    }
+
+    public void onProviderEnabled(String provider) {
+        // is provider better than bestProvider?
+        // is yes, bestProvider = provider
+        output.append("\n\nProvider Enabled: " + provider);
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        output.append("\n\nProvider Status Changed: " + provider + ", Status="
+                + S[status] + ", Extras=" + extras);
+    }
+
+    private void printProvider(String provider) {
+        LocationProvider info = locationManager.getProvider(provider);
+        output.append(info.toString() + "\n\n");
+    }
+
+    private void printLocation(Location location) {
+        if (location == null)
+            output.append("\nLocation[unknown]\n\n");
+        else
+            output.append("\n\n" + location.toString());
+    }
+
+
 
 
 }
